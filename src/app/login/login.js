47260693643058ee3b894a2ca0972f74bdbc6491
emersonnfs <rm95199@fiap.com.br>
2023-09-09 18:29:27 -0300

    initial commits

---
+++ src/app/login/login.js
@@ -0,0 +1,85 @@
+'use client'
+import Link from 'next/link';
+import React, { useState } from 'react';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState(''); 
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      email,
+      senha, 
+    };
+
+    try {
+      const response = await fetch('http://localhost:8080/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      });
+
+      if (response.ok) {
+        console.log('Login bem-sucedido');
+      } else {
+        console.error('Erro no login');
+      }
+    } catch (error) {
+      console.error('Erro na solicitação:', error);
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <div className="bg-slate-800 p-8 rounded shadow-md w-96">
+        <div>
+          <h2>STUDIT</h2>
+        </div>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <input
+              className="bg-slate-700 w-full p-2 border rounded-md"
+              type="email"
+              id="email"
+              name="email"
+              placeholder="Insira seu email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="mb-4">
+            <input
+              className="bg-slate-700 w-full p-2 border rounded-md"
+              type="password"
+              id="senha" 
+              name="senha"
+              placeholder="Insira sua senha"
+              value={senha} 
+              onChange={(e) => setSenha(e.target.value)} 
+            />
+          </div>
+          <button
+            className="w-full bg-pink-700 text-white py-2 rounded-md hover:bg-pink-700 transition duration-300"
+            type="submit"
+          >
+            Descobrir
+          </button>
+        </form>
+        <p className="mt-4">
+          Ainda não tem login?{' '}
+          <Link href="/cadastro" className="text-blue-600 hover:underline">
+            Cadastre-se
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default function LoginPagewithClient() {
+  return <LoginPage />;
+}
